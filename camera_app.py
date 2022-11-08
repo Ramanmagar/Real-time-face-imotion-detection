9e29806cdab918e828fb77da98a89afb359fb144
Raman Magar <95367709+Ramanmagar@users.noreply.github.com>
2022-11-08 13:46:34 +0530

    Update camera_app.py

--- camera_app.py
+++ camera_app.py
@@ -48,6 +48,7 @@ while True:
     #closing the app                                
     if cv2.waitKey(1) & 0xFF == ord('q') :                                       
         break
+        
 
 cap.release()
 cv2.destroyAllWindows()
